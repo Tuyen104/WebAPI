b126823d313a017ac9019d060fdf11799fdeaee4
Tuyen Le <tuyenktdn93@gmail.com>
2019-03-20 20:15:24 +0700

    unknown

---
+++ Buoi 8/JWTExample/JWTExample/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using JWTExample.Models;
+
+namespace JWTExample.Controllers
+{
+    public class LoginController : ApiController
+    {
+        JWTExampleEntities db = new JWTExampleEntities();
+        [HttpPost]
+        public HttpResponseMessage Login(User data)
+        {
+            var user = db.Users.Where(x => x.UserName.Equals(data.UserName)).SingleOrDefault();
+            if (user != null)
+            {
+                if (user.Password.Equals(data.Password))
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, TokenManager.GenerateToken(data.UserName));
+                }
+                else
+                {
+                   return Request.CreateResponse(HttpStatusCode.Forbidden, "Password is wrong");
+                }
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "The user is not exist!!!");
+            }
+        }
+        [HttpGet]
+        public HttpResponseMessage Validate(string username, string token)
+        {
+            var user = db.Users.Where(x => x.UserName.Equals(username)).SingleOrDefault();
+            if (user != null)
+            {
+                string usernameToken = TokenManager.ValidateToken(token);
+                if (user.UserName.Equals(usernameToken))
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized, "token is wrong");
+                }
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Username is not exist");
+            }
+          
+        }
+    }
+}
---
+++ Buoi 8/JWTExample/JWTExample/Models/TokenManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace JWTExample.Models
+{
+    public class TokenManager
+    {
+        private static string secret = "XCAP05H6LoKvbRRa/QkqLNMI7cOHguaRyHzyg7n5qEkGjQmtBhz4SzYh4Fqwjyi3KJHlSXKPwVu2+bXr6CtpgQ==";
+        
+        public static string GenerateToken(string username)
+        {
+            byte[] key = Convert.FromBase64String(secret);
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {
+                      new Claim(ClaimTypes.Name, username)}),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(securityKey,
+                  SecurityAlgorithms.HmacSha256Signature)
+            };
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = tokenHandler.CreateJwtSecurityToken(descriptor);
+            return tokenHandler.WriteToken(token);
+        }
+        public static ClaimsPrincipal GetPrincipal(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                JwtSecurityToken jwttoken = (JwtSecurityToken)tokenHandler.ReadToken(token);
+                if (jwttoken == null) return null;
+                byte[] key = Convert.FromBase64String(secret);
+                TokenValidationParameters parameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+                SecurityToken securityToken;
+                ClaimsPrincipal principal = tokenHandler.ValidateToken(token, parameters, out securityToken);
+                return principal;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public static string ValidateToken(string token)
+        {
+            string username = null;
+            ClaimsPrincipal principal = GetPrincipal(token);
+            if (principal == null) return null;
+            ClaimsIdentity identity = null;
+            try
+            {
+                identity = (ClaimsIdentity)principal.Identities;
+
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+            Claim usernameClaim = identity.FindFirst(ClaimTypes.Name);
+            username = usernameClaim.Value;
+            return username;
+        }
+    }
+}
